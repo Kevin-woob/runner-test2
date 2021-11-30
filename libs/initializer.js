009b46915dd0c6a301d86cfba9a0bf09944e6f6a
Kevin-woob <47277293+Kevin-woob@users.noreply.github.com>
2021-11-30 18:14:51 +0800

    clientWidth used

--- libs/initializer.js
+++ libs/initializer.js
@@ -32,8 +32,8 @@ this.setCanvasWidthHeight(canvas);
 
 	}
 	setCanvasWidthHeight(canvas){
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
+		canvas.width = window.clientWidth;
+		canvas.height = window.clientWidth;
 		return canvas;
 
 	}
